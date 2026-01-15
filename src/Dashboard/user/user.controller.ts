b37c62e7dedba83dc578f3994c60ad7c26ad2636
ahmed <ahmed.a.gad33@gmail.com>
2026-01-15 15:32:44 +0200

    update

--- src/Dashboard/user/user.controller.ts
+++ src/Dashboard/user/user.controller.ts
@@ -3,11 +3,11 @@ import { UserService } from './user.service';
 import {  CreateUserDto } from './dto';
 import { Auth } from '../../Common';
 
+@Auth('admin')
 @Controller('admin')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Auth('admin')
   @Post('create-user')
   async signup(@Body() CreateUserDto: CreateUserDto) {
     const result = await this.userService.createService(CreateUserDto);
