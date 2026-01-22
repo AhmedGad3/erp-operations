74c8bce6b4ecde38a5fe870330c64e412164dc9a
ahmed <ahmed.a.gad33@gmail.com>
2026-01-22 02:34:24 +0200

    update

--- src/Dashboard/transaction/purchase/purchase.service.ts
+++ src/Dashboard/transaction/purchase/purchase.service.ts
@@ -423,7 +423,10 @@ async findAllReturns() {
             .find()
             .sort({ returnNo: -1 })
             .populate('supplierId', 'nameAr nameEn code')
-            .populate('createdBy', 'name');
+            .populate('createdBy', 'name')
+            .populate('items.materialId', 'nameAr nameEn code')
+            .populate('items.unitId', 'nameAr nameEn symbol')
+            .exec();
     }
 
 }
