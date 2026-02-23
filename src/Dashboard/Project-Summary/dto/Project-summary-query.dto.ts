37dd5f8fc62e1cfdf0bae909265c36495798a929
ahmed <ahmed.a.gad33@gmail.com>
2026-02-23 23:01:21 +0200

    update

--- src/Dashboard/Project-Summary/dto/Project-summary-query.dto.ts
+++ src/Dashboard/Project-Summary/dto/Project-summary-query.dto.ts
@@ -1,7 +1,10 @@
-import { IsMongoId, IsOptional, IsDateString } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Types } from 'mongoose';
+import { BadRequestException } from '@nestjs/common';
 
 export class ProjectSummaryQueryDto {
-    @IsMongoId()
+    @IsNotEmpty()
+    @IsString()
     projectId: string;
 
     @IsOptional()
