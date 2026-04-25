ed3b44df9e25c22f178dde7d0dad5326b4c39978
ahmed <ahmed.a.gad33@gmail.com>
2026-04-25 22:15:47 +0300

    update

--- src/Common/Utils/send-email.utils.ts
+++ src/Common/Utils/send-email.utils.ts
@@ -1,16 +1,14 @@
 import * as nodemailer from 'nodemailer';
 
-export async function sendEmail(sendMailOptions: nodemailer.SendMailOptions) {
+export async function sendEmail(SendMailOptions: nodemailer.SendMailOptions) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
   });
 
-  await transporter.verify();
-  return transporter.sendMail(sendMailOptions);
+  return await transporter.sendMail(SendMailOptions);
 }
