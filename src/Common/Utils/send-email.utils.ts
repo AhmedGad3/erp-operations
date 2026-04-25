9a6514145d19b5b8cb462c66f627961669de9bf3
ahmed <ahmed.a.gad33@gmail.com>
2026-04-26 00:48:49 +0300

    Tighten Gmail SMTP transport config

--- src/Common/Utils/send-email.utils.ts
+++ src/Common/Utils/send-email.utils.ts
@@ -1,14 +1,23 @@
 import * as nodemailer from 'nodemailer';
 
 export async function sendEmail(SendMailOptions: nodemailer.SendMailOptions) {
+  if (!process.env.EMAIL || !process.env.PASSWORD) {
+    throw new Error('Missing EMAIL or PASSWORD env vars');
+  }
+
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
+    secure: true,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
   });
 
+  await transporter.verify();
   return await transporter.sendMail(SendMailOptions);
 }
